f1af5c9ff95afcb123242491ce29c16d486f3a08
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2025-03-15 06:20:34 +0630

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "12345678"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "XXXXXXXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "15999174"  # Replace this API ID with your actual API ID
+API_HASH = "fe9e380227e72cb214dd3348ab3ca2e8"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "7857088775:AAHUSXIamPGmA1JKMGk-bD5aO56fN15N33M"  # Replace this BOT_TOKEN
 
 # Google Api Key
-GOOGLE_API_KEY = "XXXXXXXXXXXXXXXXXXXX"  # Replace this Google Api Key
-MODEL_NAME = "gemini-1.5-flash" # Don't Change this model
+GOOGLE_API_KEY = "AIzaSyC42uG_sAZUpo08X3MfUxyBuKIzDvtSJcs"  # Replace this Google Api Key
+MODEL_NAME = "gemini-2.0-flash" # Don't Change this model
