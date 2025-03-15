eabf002d713481445ee1e054f488f5bfdb89bb20
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2025-03-15 06:59:19 +0630

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 # Pyrogram setup
 API_ID = "15999174"  # Replace this API ID with your actual API ID
 API_HASH = "fe9e380227e72cb214dd3348ab3ca2e8"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "7857088775:AAHUSXIamPGmA1JKMGk-bD5aO56fN15N33M"  # Replace this BOT_TOKEN
+BOT_TOKEN = "7576642859:AAEXe-H9GNVStKxvAujJRswGcxv-rWOOaU0"
 
 # Google Api Key
 GOOGLE_API_KEY = "AIzaSyC42uG_sAZUpo08X3MfUxyBuKIzDvtSJcs"  # Replace this Google Api Key
